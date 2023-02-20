5f809ca75cb12fce7fdef375afcd662e69dc245f
Code-MSM <mudausanaki@gmail.com>
2023-02-20 15:30:48 -0800

    Fixing_2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main - Entry point
+ * main - prints the alphabet in lowercase, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
@@ -13,7 +13,9 @@ int main(void)
 	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
+	{
 		putchar(low);
+	}
 		putchar('\n');
 
 	return (0);
