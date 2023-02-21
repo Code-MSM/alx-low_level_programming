52d331a6e9e93c91080685fb5991555dfd84b240
Code-MSM <mudausanaki@gmail.com>
2023-02-21 12:43:49 -0800

    Fixing

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,17 @@
-#include "holberton.h"
+#include <stdio.h>
 
 /**
  * print_alphabet - Check description
  * Description: It prints the alphabet in lowercase fallowed by a new line
  * Return: Nothing.
  */
-void print_alphabet(void)
+int mian(void)
 {
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
+	{
 		_putchar(i);
+	}
 	_putchar('\n');
 }
