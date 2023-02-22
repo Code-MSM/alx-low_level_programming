29709f03e3e0527303b0583d2d35870e66b8d77a
Code-MSM <mudausanaki@gmail.com>
2023-02-22 13:23:35 -0800

    Battery Stree

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *int add - a function that adds two integers and returns the result.
+ *add - a function that adds two integers and returns the result.
  *@num1: inserted number
  *@num2: inserted 2nd number
  *Return: Sum of num1 and num2
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio>
+/**
+ *print_to_98 - rints all natural numbers from n to_98
+ *@n: counts from n to_98
+ *return: Returns the counted number
+ */
+void print_to_98(int n)
+{
+	for (n = 0; n <= 98; n++)
+	{
+		return (n);
+	}
+}
