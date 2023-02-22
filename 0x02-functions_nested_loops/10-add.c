385b653dad38637a43d7366d8d477b321493d52d
Code-MSM <mudausanaki@gmail.com>
2023-02-22 08:42:01 -0800

    fixing

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -10,6 +10,6 @@ int add(int num1, int num2)
 {	int sum;
 	scanf("%d %d", &num1, &num2);
 	sum = num1 + num2;
-	printf("%d + %d = %d", num1, nu2, sum);
+	printf("%d + %d = %d", num1, num2, sum);
 	return (sum);
 }
