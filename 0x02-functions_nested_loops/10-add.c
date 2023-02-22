6dcfa4d2dfed15740480fafc66d95a7e62c1bb3f
Code-MSM <mudausanaki@gmail.com>
2023-02-22 08:34:48 -0800

    Additing numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *int add - a function that adds two integers and returns the result.
+ *@num1: inserted number
+ *@num2: inserted 2nd number
+ *Return: Sum of num1 and num2
+ */
+int add(int num1, int num2);
+{	int sum
+	scanf("%d %d", &num1, &num2);
+	sum = num1 + num2;
+	printf("%d + %d = %d", num1, nu2, sum);
+	return (sum);
+}
