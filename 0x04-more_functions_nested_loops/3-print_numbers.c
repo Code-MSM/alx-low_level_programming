d70d9f7b1eea0b1df4a134418a249ec4360a3ed4
Code-MSM <mudausanaki@gmail.com>
2023-02-24 07:24:56 -0800

    Fixing Mistakes

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  *print_numbers - a function that prints the numbers, from 0 to 9
- *@c: holds the counted number
+ *@i: holds the counted number
  *Return:0-9 followed by newline
  */
 void print_numbers(void)
 {
-	int c = 0;
+	int i = 0;
 
-	for (c; c <= 9; c++)
-	{	_putchar(c);
+	for (i; i <= 9; i++)
+	{	_putchar(i);
 	}
 	_putchar('\n');
 }
