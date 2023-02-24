d96e5ba1ffc7ee6148ff88131a5bb008b06c87d2
Code-MSM <mudausanaki@gmail.com>
2023-02-23 17:07:03 -0800

    2nd trial

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - a function that prints the numbers, from 0 to 9
+ *
+ * Return: 0-9 followed by newline
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+		_putchar(c + '0');
+	_putchar('\n');
+}
