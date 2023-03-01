aef5422faa9953ab9523e4f59a8591005728b0b8
Code-MSM <mudausanaki@gmail.com>
2023-02-28 16:53:57 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * main - check the code
- * Return: returns o0
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
-int main (void)
+int main(void)
 {
-	char *str;
+  char *str;
 
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	_puts(str);
-	return (0);
+  str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+  _puts(str);
+  return (0);
 }
